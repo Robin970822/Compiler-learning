50cd5639c0601af6f988ecf7edaf4c3f3dd35f31
Robin970822 <717740827@qq.com>
2018-06-02 16:19:58 +0800

    Build State and TokenType.

---
+++ src/common/State.java
@@ -0,0 +1,24 @@
+package common;
+
+/**
+ *  状态常量
+ */
+public enum State {
+    // 开始状态常量
+    ST,
+
+    // 标识符状态常量
+    UN,
+    IL,
+
+    // 浮点数状态常量
+    PL,
+    EQ,
+    FY,
+    DO,
+    AG,
+    MI,
+    ET,
+    TW,
+    DONE
+}
---
+++ src/common/TokenType.java
@@ -0,0 +1,51 @@
+package common;
+
+/**
+ *  字符类型
+ */
+public enum TokenType{
+    // 专用符号
+    PLUS,           //+
+    MINUS,          //-
+    EQUAL,          //=
+    LBRACE,         //{
+    RBRACE,         //}
+    COLON,          //:
+    SEMI,           //;
+    SPACE,          //
+    TABLE,          //
+    ENTER,          //
+    DOUBLECOLON,    //::
+    PLUSEQUALTO,    //+=>
+    MINUSTO,        //->
+    EQUALTO,        //=>
+
+    ERROR,
+    ENDFILE,
+    NULL,
+
+    //
+    IDENTIFIER,     //标识符
+    DECIMAL,        //实数
+
+    // 关键字
+    THREAD,
+    FEATURES,
+    FLOWS,
+    PROPERTIES,
+    END,
+    NONE,
+    IN,
+    OUT,
+    DATA,
+    PORT,
+    EVENT,
+    PARAMETER,
+    FLOW,
+    SOURCE,
+    SINK,
+    PATH,
+    CONSTANT,
+    ACCESS,
+    INOUT
+}
